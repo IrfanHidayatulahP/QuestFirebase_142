7bbf93358ace0e8b79074cd35b64831b371e3112
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-01-11 23:15:13 +0700

    membuat data class InsertUiState, FormErrorState, dan FormState di InsertViewModel

--- app/src/main/java/com/example/pamfirebase/ui/viewmodel/InsertViewModel.kt
+++ app/src/main/java/com/example/pamfirebase/ui/viewmodel/InsertViewModel.kt
@@ -10,6 +10,32 @@ class InsertViewModel (
 
 }
 
+sealed class FormState {
+    object Idle : FormState()
+    object Loading : FormState()
+    data class Succes(val message: String) : FormState()
+    data class Error(val message: String) : FormState()
+}
+
+data class FormErrorState (
+    val nim: String? = null,
+    val nama: String? = null,
+    val jenisKelamin: String? = null,
+    val alamat: String? = null,
+    val kelas: String? = null,
+    val angkatan: String? = null
+) {
+    fun isValid() : Boolean {
+        return nim == null && nama == null && jenisKelamin == null &&
+                alamat == null && kelas == null && angkatan == null
+    }
+}
+
+data class InsertUiState (
+    val insertUiEvent: MahasiswaEvent = MahasiswaEvent(),
+    val isEntryValid: FormErrorState = FormErrorState(),
+)
+
 // data class Variable yang menyimpan data input form
 data class MahasiswaEvent(
     val nim: String = "",
