65f7f0cead20143ff294ac45e27926e41321d320
IrfanHidayatulahP <irfanhputra400@gmail.com>
2025-01-12 00:46:14 +0700

    membuat fungsi PengelolaHalaman yang berisi untuk memanggil halaman HomeScreen dan InsertView

---
+++ app/src/main/java/com/example/pamfirebase/ui/navigation/PengelolaHalaman.kt
@@ -0,0 +1,39 @@
+package com.example.pamfirebase.ui.navigation
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.pamfirebase.ui.view.InsertView
+import com.umy.pam_firebase.ui.pages.HomeScreen
+
+@Composable
+fun PengelolaHalaman(
+    modifier: Modifier,
+    navController: NavHostController = rememberNavController()
+) {
+    NavHost(
+        navController = navController,
+        startDestination = DestinasiHome.route,
+        modifier = Modifier
+    ) {
+        composable(DestinasiHome.route) {
+            HomeScreen(
+                navigateToItemEntry = {
+                    navController.navigate(DestinasiInsert.route)
+                },
+            )
+        }
+
+        composable(DestinasiInsert.route) {
+            InsertView(
+                onBack = { navController.popBackStack() },
+                onNavigate = {
+                    navController.navigate(DestinasiHome.route)
+                }
+            )
+        }
+    }
+}
